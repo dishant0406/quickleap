f44b7d98063a403cce622c11ed5aa730a6980128
Dishant <--local>
2025-10-11 11:14:40 +0530

    refactor: simplify NEXT_PUBLIC_LAZYWEB_BACKEND_URL assignment

--- lib/env/index.ts
+++ lib/env/index.ts
@@ -2,8 +2,7 @@ export const env = {
   // API Endpoints
   NEXT_PUBLIC_WEBSITE_SCREENSHOT_API_ENDPOINT:
     process.env.NEXT_PUBLIC_WEBSITE_SCREENSHOT_API_ENDPOINT || 'https://api.lazyweb.rocks/ss',
-  NEXT_PUBLIC_LAZYWEB_BACKEND_URL:
-    process.env.NEXT_PUBLIC_LAZYWEB_BACKEND_URL || 'https://redirect.quickleap.io',
+  NEXT_PUBLIC_LAZYWEB_BACKEND_URL: 'https://redirect.quickleap.io',
   NEXT_PUBLIC_META_DATA_ENDPOINT:
     process.env.NEXT_PUBLIC_META_DATA_ENDPOINT || 'https://api.lazyweb.rocks/metadata',
 
